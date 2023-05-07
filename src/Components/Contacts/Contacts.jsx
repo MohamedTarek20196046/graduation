7a79a6722afbfc366363e82693733617920eeb75
MohamedTarek <mohamedtarek20196046@gmail.com>
2023-05-08 00:36:07 +0300

    media Screen

--- src/Components/Contacts/Contacts.jsx
+++ src/Components/Contacts/Contacts.jsx
@@ -3,8 +3,26 @@ import styles from './Contacts.module.css'
 export default function Contacts() {
   return (
     <>
-     <section id="contact" className={styles.contact}>
+     <section id="contact" className={`${styles.contact} ${styles.display1}`}>
         <div className="container  w-50 text-center p-4 ">
+            <h3 className="text-center my-md-4">Contact Us</h3>
+            <form action="" className="rounded p-4">
+                <div className="row gy-3">
+                    <div className="col-md-6"> <input className={`form-control ${styles.formControlBg}`} type="text" placeholder="Enter Name"/></div>
+
+                    <div className="col-md-6"><input className={`form-control ${styles.formControlBg}`} type="text" placeholder="Enter Email"/></div>
+                    <div className="col-md-12">
+                        <textarea className={`form-control ${styles.formControlBg}`} placeholder="Enter your complaint" rows="7"></textarea>
+                    </div>
+                </div>
+                <button className={`btn btn-info text-white mt-4 ${styles.submitBtn} rounded-pill`}>submit</button>
+            </form>
+        </div>
+    </section>
+    
+    {/* Mobile view */}
+    <section id="contact" className={`${styles.contact} ${styles.display2}`}> 
+        <div className="container   text-center p-4 ">
             <h3 className="text-center my-md-4">Contact Us</h3>
             <form action="" className="rounded p-4">
                 <div className="row gy-3">
