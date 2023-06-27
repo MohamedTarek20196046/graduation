5f2ab046461ce428040a02127b6cc2e755c38b69
MohamedTarek20196046 <94565639+MohamedTarek20196046@users.noreply.github.com>
2023-06-28 02:45:52 +0300

    Update StaticTracking.jsx

--- src/Components/StaticTracking/StaticTracking.jsx
+++ src/Components/StaticTracking/StaticTracking.jsx
@@ -79,7 +79,7 @@ export default function StaticTracking() {
   
     function test()
     {
-      axios.get('http://16.171.39.16/test')
+      axios.get('https://16.171.39.16/test')
       .then(response => {
         console.log(response.data);
       })
@@ -94,7 +94,7 @@ export default function StaticTracking() {
       const formData = new FormData();
       formData.append('image', file);
     
-      fetch('http://16.171.39.16/detect_image', {
+      fetch('https://16.171.39.16/detect_image', {
         method: 'POST',
         body: formData,
       })
