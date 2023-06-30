10a855364170e76fa3185241d5858c41a946513a
Mohamed Tarek <94565639+MohamedTarek20196046@users.noreply.github.com>
2023-06-30 04:04:31 +0300

    Update LiveTracking.jsx

--- src/Components/LiveTracking/LiveTracking.jsx
+++ src/Components/LiveTracking/LiveTracking.jsx
@@ -69,7 +69,107 @@ export default function LiveTracking() {
      
     }
 
+    const videoRef = useRef(null);
+  const [error, setError] = useState(null);
+  const [latestFrame, setLatestFrame] = useState(null);
+  const [latestJson, setLatestJson] = useState(null);
+  const [streaming, setStreaming] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
 
+  const startStream = async () => {
+    try {
+      const video = videoRef.current;
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      video.srcObject = stream;
+      video.play();
+      setStreaming(true);
+
+      const id = setInterval(async () => {
+        try {
+          const imageBlob = await getImageFromStream(stream);
+          const formData = new FormData();
+          formData.append('image', imageBlob);
+          const response = await axios.post(
+            'https://terfci.msp-asu.tech/detect_image',
+            formData,
+            {
+              headers: { 'Content-Type': 'multipart/form-data' },
+              responseType: 'json',
+            }
+          );
+          setLatestFrame(`data:image/jpeg;base64,${response.data.image}`);
+          setLatestJson(response.data);
+        } catch (error) {
+          setError(error);
+        }
+      }, 200);
+
+      setIntervalId(id);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const stopStream = () => {
+    clearInterval(intervalId);
+    const stream = videoRef.current?.srcObject;
+    if (stream) {
+      stream.getTracks().forEach((track) => track.stop());
+    }
+    setLatestFrame(null);
+    setLatestJson(null);
+    setStreaming(false);
+    setIntervalId(null);
+  };
+
+  useEffect(() => {
+    let id;
+
+    const start = async () => {
+      id = await startStream();
+    };
+
+    const stop = () => {
+      stopStream();
+    };
+
+    return () => {
+      stop();
+    };
+  }, []);
+
+  const getImageFromStream = (stream) => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = 640;
+    canvas.height = 480;
+    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(
+        (blob) => {
+          if (blob) {
+            resolve(blob);
+          } else {
+            reject(new Error('Failed to get image from stream'));
+          }
+        },
+        'image/jpeg',
+        0.8
+      );
+    });
+  };
+
+  const handleStartStream = () => {
+    if (!streaming) {
+      startStream();
+    }
+  };
+
+  const handleStopStream = () => {
+    if (streaming) {
+      stopStream();
+    }
+  };
     function sound(){
         const utterance = new SpeechSynthesisUtterance("Live tracking");
         speechSynthesis.speak(utterance);
@@ -91,106 +191,31 @@ export default function LiveTracking() {
     <FloatButton onClick={toggleListen} icon={<AudioOutlined />}/>
    <TrackNav/>
    <AnimatedPage>
-   <div className={`${styles.tracking} container ${styles.display1}`}>
-    
+      <div className={`${styles.tracking} container`}>
         <div className="text-center w-75 m-auto">
-            <p className="text-white fs-3 text-center">Live tracking is a dynamic service where the user will be able to
-                open the camera and detect lane, Sign, traffic lights, crosswalks and pedestrians while your camera is
-                rolling. </p>
+          <p className="text-white fs-3 text-center">
+            Live tracking is a dynamic service where the user will be able to
+            open the camera and detect lane, Sign, traffic lights, crosswalks and pedestrians while your camera is
+            rolling. </p>
         </div>
 
         <div className={`${styles.camera} m-auto d-flex justify-content-center align-items-center`}>
-            <i className={`fa-solid fa-camera ${styles.cameraIcon}`}></i>
-            
-        </div>
-
-        <div className={`${styles.check} m-auto p-3 mt-4 text-white`}>
-            <p>Choose the objects you want to detect:</p>
-            <form>
-                <label>
-                    <input type="checkbox" name="lane" value="lane"/> Lane
-                </label>
-                <br/>
-                <label>
-                    <input type="checkbox" name="sign" value="sign"/> Sign
-                </label>
-                <br/>
-
-                <label>
-                    <input type="checkbox" name="Trafficlights" value="Traffic Lights"/> Traffic Lights
-                </label>
-                <br/>
-                <label>
-                    <input type="checkbox" name="Crosswalks" value="Crosswalks"/> Crosswalks
-                </label>
-                <br/>
-                <label>
-                    <input type="checkbox" name="Pedestrians" value="Pedestrians"/> Pedestrians
-                </label>
-            </form>
+          
+          {/* {error && <div>{error.message}</div>} */}
+          <video ref={videoRef} className={`${styles.cameraIcon} d-none`}  />
+          {latestFrame && <img src={latestFrame} />}
+          
         </div>
 
         <div className=" m-auto mt-md-4 w-50 p-4 d-flex justify-content-center">
-            <button onClick={sound} className={`btn p-2  ${styles.trackBtn} me-3`}>Start Tracking</button>
-            <button className={`btn p-2  ${styles.trackBtn} me-3`}>Stop Tracking</button>
-        </div>
-    </div>
-
-
-    {/* Mobile view */}
-
-    <div className={`${styles.tracking}  ${styles.display2}`}>
-        <div className="text-center w-75 pt-3 pb-1 m-auto">
-            <p className={`text-white ${styles.font} text-center`}>Live tracking is a dynamic service where the user will be able to
-                open the camera and detect lane, Sign, traffic lights, crosswalks and pedestrians while your camera is
-                rolling. </p>
+          <button className={`btn p-2  ${styles.trackBtn} me-3`} onClick={handleStartStream} disabled={streaming}>Start Tracking</button>
+          <button className={`btn p-2  ${styles.trackBtn} me-3`} onClick={handleStopStream} disabled={!streaming}>Stop Tracking</button>
         </div>
 
-        <div className={`${styles.camera} m-auto d-flex justify-content-center align-items-center`}>
-            <i className={`fa-solid fa-camera ${styles.cameraIcon}`}></i>
-            
-        </div>
-
-        <div className={`${styles.check} m-auto p-3 mt-4 text-white`}>
-            <p>Choose the objects you want to detect:</p>
-            <form>
-                <label>
-                    <input type="checkbox" name="lane" value="lane"/> Lane
-                </label>
-                <br/>
-                <label>
-                    <input type="checkbox" name="sign" value="sign"/> Sign
-                </label>
-                <br/>
-
-                <label>
-                    <input type="checkbox" name="Trafficlights" value="Traffic Lights"/> Traffic Lights
-                </label>
-                <br/>
-                <label>
-                    <input type="checkbox" name="Crosswalks" value="Crosswalks"/> Crosswalks
-                </label>
-                <br/>
-                <label>
-                    <input type="checkbox" name="Pedestrians" value="Pedestrians"/> Pedestrians
-                </label>
-            </form>
-        </div>
-
-        <div className=" m-auto mt-md-4 w-50 p-4 d-flex justify-content-center">
-            <button className={`btn p-2  ${styles.trackBtn} me-3`}>Start Tracking</button>
-            <button className={`btn p-2  ${styles.trackBtn} me-3`}>Stop Tracking</button>
-        </div>
-    </div>
-
-
-
-
-
-
-
-
-    <Footer/>
+        {/* {latestJson && <pre>{JSON.stringify(latestJson, null, 2)}</pre>} */}
+      
+      </div>
+      <Footer />
     </AnimatedPage>
    </>
   )
