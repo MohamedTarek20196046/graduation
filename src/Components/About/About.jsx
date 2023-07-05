48296389c119b19e480c99b31c5a625fefc00ae0
Mohamed Tarek <94565639+MohamedTarek20196046@users.noreply.github.com>
2023-07-06 01:27:38 +0300

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <section className={`${styles.about}`} id="about">
             <div className="container text-center p-4 ">
                 <h3 className="text-center my-md-4">About Us</h3>
-                <p className="text-white fs-5">Our1 driver assistance app uses your smartphone's camera and internet to detect traffic elements like cars, lights, and signs as you drive. The app then alerts you if you approach a stop sign or red light without slowing down. Our goal is to provide an extra layer of safety awareness for drivers by helping them catch things that they may miss.</p>
+                <p className="text-white fs-5">Our driver assistance app uses your smartphone's camera and internet to detect traffic elements like cars, lights, and signs as you drive. The app then alerts you if you approach a stop sign or red light without slowing down. Our goal is to provide an extra layer of safety awareness for drivers by helping them catch things that they may miss.</p>
             </div>
         </section>
     </>
